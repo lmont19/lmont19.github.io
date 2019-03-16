7620e5c03b659ed653af8c2b6ccef37e5a2324de
lmont19 <montanez.luis19@gmail.com>
2019-03-16 07:22:04 +0300

    complete makeover

--- portfolio.js
+++ portfolio.js
@@ -1,30 +1,26 @@
 $(document).ready(function(){
 
-var textArr = ['Web Developer', 'Fitness Enthusiast', 'Photographer', 'Self Learner', 'Web Developer', 'Fitness Enthusiast', 'Photographer', 'Self Learner' ]
-var index = 0;
-setInterval(function(){        
-$("#changeText").animate({
-opacity:0
-},function()
-{
-   if(textArr.length > index) {
-   $(this).text(textArr[index]).animate({opacity:1})
-   index++; 
-   }
-   else
-   index = 0;
-});
-},2000);
+  var app = document.getElementById('app');
+
+  var typewriter = new Typewriter(app, {
+      loop: true
+  });
+  
+  typewriter.typeString('Welcome!')
+    .pauseFor(2500)
+    .deleteAll()
+    .typeString('I am Luis Montanez!')
+    .pauseFor(2500)
+    .deleteAll()
+    .typeString('I am a Full Stack Web Developer!')
+    .pauseFor(2500)
+    .deleteAll()
+    .typeString('Feel free to look around...')
+    .pauseFor(2500)
+    .deleteAll()
+    .typeString('Drop me a line for collabs!')
+    .start();
+
+}); 
 
 
-var $grid = $('.grid').masonry({
-   itemSelector: '.grid-item',
-   percentPosition: true,
-   columnWidth: '.grid-sizer'
- });
- // layout Masonry after each image loads
- $grid.imagesLoaded().progress( function() {
-   $grid.masonry();
- });  
-
-});
