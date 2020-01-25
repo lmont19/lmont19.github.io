7e8d00434232eb86929aa0f644273241c97f0d34
lmont19 <montanez.luis19@gmail.com>
2020-01-25 18:36:02 -0500

    Added react-router

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -4,7 +4,7 @@ import profilepic from "../img/profilepic.jpeg";
 function Navbar() {
   return (
     <nav id="navbar">
-      <img src={profilepic} />
+      <img src={profilepic} alt="" />
       <a href="#aboutme">About Me</a>
       <a href="#projects">Projects</a>
       <a href="https://github.com/lmont19" target="_blank" id="profile-link">
