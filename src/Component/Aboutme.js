519a1fc76ba74cad6bcce57160d3f8aec8f91bdc
lmont19 <montanez.luis19@gmail.com>
2020-01-29 19:40:09 -0500

    css mods

--- src/Component/Aboutme.js
+++ src/Component/Aboutme.js
@@ -3,12 +3,35 @@ import React from "react";
 function AboutMe() {
   return (
     <div id="about-me">
+      <div id="info-div">
+        <p>
+          {" "}
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam dolor
+          necessitatibus id facere eius aut dicta enim obcaecati itaque
+          perferendis, quam, ipsam dolorem. Repellendus enim dolor, similique
+          aliquam optio placeat laborum earum repellat ea aut fugit saepe
+          expedita perspiciatis accusamus, neque modi reiciendis soluta
+          repudiandae ab numquam ipsam rerum omnis labore? Voluptas non earum
+          numquam perspiciatis doloribus reprehenderit debitis, laborum
+          voluptates! Necessitatibus error modi dicta, eius cum laudantium
+          consequatur iste incidunt numquam suscipit quidem quos ipsum, sint
+          quaerat odio commodi optio magnam. Amet accusamus magni culpa quae
+          dolores molestiae, aut facilis ratione ullam eius nulla assumenda
+          asperiores vitae voluptate accusantium quod velit aliquid odit
+          incidunt dicta eos numquam, eum alias explicabo. Inventore quibusdam
+          ipsam nam temporibus nobis perferendis, deserunt veritatis
+          praesentium. Ipsa earum cumque nemo voluptate maiores reiciendis in
+          asperiores, ad minus, aliquid nam. Incidunt sunt similique quos
+          commodi repudiandae et doloremque harum, facere, dicta voluptates
+          necessitatibus corrupti, voluptatum doloribus?{" "}
+        </p>
+      </div>
       <div id="social-media">
         <span>
-          <i class="fab fa-github fa-2x"></i>
+          <i className="fab fa-github fa-2x"></i>
         </span>
         <span>
-          <i class="far fa-envelope fa-2x"></i>
+          <i className="far fa-envelope fa-2x"></i>
         </span>
       </div>
     </div>
